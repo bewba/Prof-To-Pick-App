06eab11f247b28efe0a5a44a369d36e2b7010934
bewba <lumaguihans0123@gmail.com>
2023-12-10 01:05:17 +0800

    I dont know why this would be my first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+
+let click = document.getElementById("button")
+const niggaButton = document.addEventListener("click",printHello)
+
+if (niggaButton) {
+    printHello();
+    console.log(click)
+}
+
+function printHello() {
+    console.log("sad")
+}
