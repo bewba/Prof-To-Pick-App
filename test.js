4fa94210d53bc4e61e2b6e4401e2c669407d6b3b
bewba <lumaguihans0123@gmail.com>
2023-12-12 14:50:17 +0800

    added star ratings

--- test.js
+++ test.js
@@ -7,3 +7,26 @@ const buttonEffect = document.addEventListener("fancyButton",printHello)
 function printHello() {
     console.log("sad")
 }
+
+const stars = document.querySelectorAll('.star');
+
+// Add click event listener to each star
+stars.forEach(star => {
+    star.addEventListener('click', () => {
+        const rating = parseInt(star.getAttribute('data-value'));
+        document.getElementById('profRating').value = rating;
+        highlightStars(rating);
+    });
+});
+
+// Function to highlight stars up to the selected rating
+function highlightStars(rating) {
+    stars.forEach(star => {
+        const value = parseInt(star.getAttribute('data-value'));
+        if (value <= rating) {
+            star.classList.add('selected');
+        } else {
+            star.classList.remove('selected');
+        }
+    });
+}
